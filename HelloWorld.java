4c8f7c4d51da020f1f8e000a85cbc081c532a615
5ia14gicardillo <5ia14gicardillo@gibwintra.ch>
2016-09-20 08:32:45 +0200

    Constructor added for no reason

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld
 {
+	public HelloWorld(){
+		// Just an empty constructor for no reason
+	}
+	
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
