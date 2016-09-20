31fd5524c63537986e0df754e474828d1b0d9cea
5ia14gicardillo <5ia14gicardillo@gibwintra.ch>
2016-09-20 08:47:27 +0200

    Class formatted

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
-public class HelloWorld
-{
-	public HelloWorld(){
+public class HelloWorld {
+
+	public HelloWorld() {
 		// Just an empty constructor for no obvious reason
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
